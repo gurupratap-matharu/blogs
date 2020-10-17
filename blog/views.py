be9efacdfaec154b9bba58bb63568f80853511ea
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-17 10:41:40 -0300

    added class based view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from blog.models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+    context_object_name = "posts"
 
 
 def post_list(request):
