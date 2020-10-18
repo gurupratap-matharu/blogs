313d2a4e7d8c16f97e8b07acdf1f24a03894c617
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-18 12:46:40 -0300

    using detail view with form mixing aint that cool?

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import FormView, ListView
+import logging
 
-from blog.forms import EmailPostForm
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, FormView, ListView
+from django.views.generic.edit import FormMixin
+
+from blog.forms import CommentForm, EmailPostForm
 from blog.models import Post
+
+logger = logging.getLogger(__name__)
 
 
 class PostListView(ListView):
@@ -12,22 +19,46 @@ class PostListView(ListView):
     context_object_name = "posts"
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        slug=post,
-        status="published",
-        publish__year=year,
-        publish__month=month,
-        publish__day=day,
-    )
-    return render(request, "blog/post/detail.html", {"post": post})
+class PostDetailView(FormMixin, DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/post/detail.html"
+    success_message = "Comment added successfully!"
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return self.get_object().get_absolute_url()
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.post = self.get_object()
+        form.save()
+        messages.success(self.request, self.success_message)
+        logger.info("saved comment with form %s", form)
+        return super().form_valid(form)
+
+    def get_object(self):
+        return get_object_or_404(
+            Post,
+            slug=self.kwargs["post"],
+            status="published",
+            publish__year=self.kwargs["year"],
+            publish__month=self.kwargs["month"],
+            publish__day=self.kwargs["day"],
+        )
 
 
-class PostShare(FormView):
+class PostShare(SuccessMessageMixin, FormView):
     form_class = EmailPostForm
     template_name = "blog/post/share.html"
-    success_url = "/thanks/"
+    success_url = "/"
+    success_message = "Post shared successfully!"
 
     def form_valid(self, form):
         post = get_object_or_404(Post, id=self.kwargs["post_id"], status="published")
