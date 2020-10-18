aefbaa9a33fef8dab6e3ed2df4be6759feea6da2
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-18 12:46:20 -0300

    using CBV instead for FBV

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
-        views.post_detail,
+        views.PostDetailView.as_view(),
         name="post_detail",
     ),
     path("<int:post_id>/share/", views.PostShare.as_view(), name="post_share"),
