938c22c9eae985b63b3079072898c0bdd9545b10
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-27 15:20:32 -0300

    added new endpoint for cbv

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
         name="post_detail",
     ),
     path("<int:post_id>/share/", views.PostShare.as_view(), name="post_share"),
+    path("tag/<slug:tag_slug>/", views.PostListView.as_view(), name="post_list_by_tag"),
 ]
