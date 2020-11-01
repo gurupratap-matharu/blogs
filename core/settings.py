5dff662df65c1f7ebbc41e8693b3a18fd03ea1ab
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-01 16:51:18 -0300

    added sitemaps to installed apps

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    "django.contrib.sitemaps",
     # 3rd party
     "allauth",
     "allauth.account",
